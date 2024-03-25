21834b2f2a41cc336c41e858ccfb0303b74642c6
Yeogaeunn <163220348+Yeogaeunn@users.noreply.github.com>
2024-03-25 17:13:27 +0900

    Update FactorialDemo1.java

--- FactorialDemo1.java
+++ FactorialDemo1.java
@@ -21,3 +21,29 @@ public class FactorialDemo1 {
 	}
 
 }
+
+while(true)로 수정한 후 다시 실행
+
+	
+package javaproject;
+
+import java.util.Scanner;
+
+public class FactorialDemo1 {
+	public static void main(String[] args) {
+		int result;
+		int n;
+		Scanner in = new Scanner (System.in);
+		
+		System.out.print("팩토리얼 값을 구할 정수 : ");
+		n = in.nextInt();
+		
+		result = 1;
+		while (true) {
+			if (n > 0)
+				result *= n--;
+			else if ( n <= 0)
+				break;
+		}
+		System.out.println(result);
+}
