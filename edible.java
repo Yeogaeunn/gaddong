0deee1b44193b0522b224953ee468c5b557687e1
Yeogaeunn <163220348+Yeogaeunn@users.noreply.github.com>
2024-06-12 14:45:46 +0900

    Create edible.java//chapter 7 p.302-2

---
+++ edible.java
@@ -0,0 +1,12 @@
+interface Edible{
+    void eat();
+}
+
+interface Sweetable{
+    void sweet();
+}
+
+interface Delicious extends Edible, Sweetable{
+}
+[출처] [6,7장] 쉽게 배우는 자바 프로그래밍 - 도전과제 & 프로그래밍 문제|작성자 박예림
+
